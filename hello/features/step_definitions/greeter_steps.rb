4722e076cebdb1d326efdb173a783067179384dc
kano4 <shinji@kano4.com>
2011-08-05 16:51:59 +0900

    p035 add CucumberGreeter class

--- hello/features/step_definitions/greeter_steps.rb
+++ hello/features/step_definitions/greeter_steps.rb
@@ -1,9 +1,15 @@
+class CucumberGreeter
+  def greet
+    "Hello Cucumber!"
+  end
+end
+
 Given /^a greeter$/ do
   @greeter = CucumberGreeter.new
 end
 
 When /^I send it the greet message$/ do
-  @message = @greeter.great
+  @message = @greeter.greet
 end
 
 Then /^I should see "([^"]*)"$/ do |greeting|
