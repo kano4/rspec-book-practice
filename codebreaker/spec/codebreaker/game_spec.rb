058b18c57d96cc07191cef90fe7ba3a224541237
kano4 <shinji@kano4.com>
2011-08-08 20:36:36 +0900

    p71 modify game_spec.rb

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -4,7 +4,7 @@ module Codebreaker
   describe Game do
     describe "#start" do
       it "sends a welcome message" do
-        output = double('output')
+        output = double('output').as_null_object
         game = Game.new(output)
 
         output.should_receive(:puts).with('Welcome to Codebreaker!')
@@ -13,7 +13,7 @@ module Codebreaker
       end
 
       it "prompts for the first guess" do
-        output = double('output')
+        output = double('output').as_null_object
         game = Game.new(output)
 
         output.should_receive(:puts).with('Enter guess:')
